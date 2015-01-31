55d49063d5582e0e8e94f4b8ca5414f46baa6000
anxingle <anxingle@163.com>
2015-01-31 12:10:30 +0800

    Init Project
    This is a PHP init project!

---
+++ Ask/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'DB_TYPE'=>'mysql',
+    'DB_HOST'=>'localhost',
+    'DB_NAME'=>'exam',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'panpan',
+    'DB_PORT'=>'3306',
+    'DB_PREFIX'=>'sel_',
+);
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+    define('APP_DEBUG',false);
+    define('APP_NAME','Ask');
+    define('APP_PATH','./Ask/');
+    require './ThinkPHP/ThinkPHP.php';   
